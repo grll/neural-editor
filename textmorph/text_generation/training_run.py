cbce0a100841e1b4738f6e041adc4e7db3e0211e
Guillaume Raille <guillaume.raille@gmail.com>
2018-10-16 14:15:30 +0200

    one fixed modification for generation

--- textmorph/text_generation/training_run.py
+++ textmorph/text_generation/training_run.py
@@ -105,10 +105,10 @@ class GenData(object):
 
             with codecs.open(path, 'r', encoding='utf-8') as f:
                 for line in verboserate(f, desc='Reading data file.', total=total_lines):
-                    src, _ = line.strip().lower().split('\t')
+                    src = line.strip().lower()
                     src_words = src.split(' ')
                     assert len(src_words) > 0
-                    examples.append(EditExample(source_words=src_words, insert_words=[], insert_exact_words=[], delete_words=[], delete_exact_words=[], target_words=[], edit_embed=None))
+                    examples.append(EditExample(source_words=src_words, insert_words=[], insert_exact_words=[u'war'], delete_words=[], delete_exact_words=[u'bin'], target_words=[], edit_embed=None))
             return examples
 
         self.data = examples_from_file(join(data_dir, file_name))
