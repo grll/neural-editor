12f4637d97935869a92c354a98c9a0c0c89e9cb0
Guillaume Raille <guillaume.raille@gmail.com>
2018-10-25 17:47:58 +0200

    start ssh service from the init.sh script

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ echo "source /etc/environement" >> /etc/mypython.sh
 echo "/opt/conda/envs/pytorch-py27/bin/python \"$@\"" >> /etc/mypython.sh
 chmod 755 /etc/mypython.sh
 env >> etc/environement
+service ssh start
