394f229735e5d66b747adbc7a319a83775f43ec4
Guillaume Raille <guillaume.raille@gmail.com>
2018-10-25 17:56:39 +0200

    correct typo

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ echo "#!/bin/bash -l" >> /etc/mypython.sh
 echo "source /etc/environement" >> /etc/mypython.sh
 echo "/opt/conda/envs/pytorch-py27/bin/python \"$@\"" >> /etc/mypython.sh
 chmod 755 /etc/mypython.sh
-touch etc/environement
-env >> etc/environement
+touch /etc/environement
+env >> /etc/environement
 service ssh start
